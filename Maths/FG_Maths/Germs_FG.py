635cf41c4942a82715c49d4c7aaabcd2002eb691
Labardini <34993699+Labardini@users.noreply.github.com>
2021-07-20 14:48:00 -0500

    Add files via upload

--- Maths/FG_Maths/Germs_FG.py
+++ Maths/FG_Maths/Germs_FG.py
@@ -1,1 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 17 17:07:25 2017
 
+@author: daniellabardini
+"""
+
+import numpy
+
+
+
+from exception_handling import myInputError
+from Maths.CP_Maths import extended_complex_plane_CP
+from Maths.CP_Maths import Steiner_grids_CP
+
+
+#### SOME FUNCTIONS IMPORTED FROM Maths.CP_Maths.extended_complex_plane_CP
+oo = extended_complex_plane_CP.numpyExtendedComplexPlane().oo
+isooInArgs = extended_complex_plane_CP.numpyExtendedComplexPlane().isooInArgs
+isooInList = extended_complex_plane_CP.numpyExtendedComplexPlane().isooInList
+extendedValue = extended_complex_plane_CP.numpyExtendedComplexPlane().extendedValue
+areAllDistinctArgs = extended_complex_plane_CP.numpyExtendedComplexPlane().areAllDistinctArgs
+areAllDistinctList = extended_complex_plane_CP.numpyExtendedComplexPlane().areAllDistinctList
+removeooFromArgs = extended_complex_plane_CP.numpyExtendedComplexPlane().removeooFromArgs
+removeooFromList = extended_complex_plane_CP.numpyExtendedComplexPlane().removeooFromList
+e_circumcenter_and_radius = extended_complex_plane_CP.numpyExtendedComplexPlane().e_circumcenter_and_radius
+areCollinear = extended_complex_plane_CP.numpyExtendedComplexPlane().areCollinear
+####
+
+
+
+
+#############################
+
+class AnalyticContinuation:
+    
+    def __init__(self):
+        pass
