9f38e26790a022e19f682aaec683e26ed8cbc645
jadl98 <80933419+jadl98@users.noreply.github.com>
2021-08-28 17:05:54 -0500

    Add files via upload
    Corrección 28/08/21

--- curvesAndDrawings/circle_segments.py
+++ curvesAndDrawings/circle_segments.py
@@ -11,36 +11,65 @@ from PyQt5 import QtGui#, QtCore
 import numpy
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore
+from BackgroundControl.background_control import choosingBackground
 
-# Maybe it would be useful to transofrm cicle_segments into a curvesAndDrawings module with some other basic shapes
 
 class circSegment:
-
-    def __init__(self,center_xcoord,center_ycoord,radius,startAngle,endAngle):
+    
+    def __init__(self,center_xcoord,center_ycoord,radius,startAngle,endAngle,puntos=500,backgroundColor="Black",circSegmentColor="white"):
         #startAngle -> clockwise -> endAngle
         #startAngle AND endAngle are input in radians
+        backgroundColor = choosingBackground(backgroundColor)
+        exec("self.color =backgroundColor." + circSegmentColor) 
+        exec("self.white =backgroundColor." + circSegmentColor) 
+        self.black = backgroundColor.black
         self.center_xcoord = center_xcoord
         self.center_ycoord = center_ycoord
         self.radius = radius
-        self.startAngle = 16*180*startAngle/numpy.pi # THE UNIT IN QtGui.QGraphicsEllipseItem IS 1/16 OF A DEGREE
-        self.endAngle = 16*180*endAngle/numpy.pi # THE UNIT IN QtGui.QGraphicsEllipseItem IS 1/16 OF A DEGREE
+        self.startAngle = -16*180*startAngle/numpy.pi # THE UNIT IN QtGui.QGraphicsEllipseItem IS 1/16 OF A DEGREE
+        self.endAngle = -16*180*endAngle/numpy.pi # THE UNIT IN QtGui.QGraphicsEllipseItem IS 1/16 OF A DEGREE
         self.spanAngle = self.endAngle - self.startAngle
         self.lowerLeftCorner_xcoord = self.center_xcoord - self.radius
         self.lowerLeftCorner_ycoord = self.center_ycoord - self.radius
+        
+        # self.startAngleRad = startAngle
+        # self.endAngleRad = endAngle
+        # self.puntosADibujar = puntos
+        # self.x_coord = self.center_xcoord + self.radius*numpy.cos(numpy.linspace(self.startAngleRad,self.endAngleRad,self.puntosADibujar))
+        # self.y_coord=  self.center_ycoord + self.radius*numpy.sin(numpy.linspace(self.startAngleRad,self.endAngleRad,self.puntosADibujar))
+        # self.goodSegment = pg.PlotCurveItem(self.x_coord,self.y_coord, pen=pg.mkPen('k',width=2.5), clickable=True)
+        # self.complementarySegment = self.goodSegment
 
-        self.goodSegment = QtGui.QGraphicsEllipseItem(QtCore.QRectF(self.lowerLeftCorner_xcoord, self.lowerLeftCorner_xcoord, 2*self.radius, 2*self.radius))
-        self.goodSegment.setPen(pg.mkPen(color = 'k',width=2.5))
-        # self.PlotWidgetIn_pageUHP.addItem(self.goodSegment)
+        self.goodSegment = QtGui.QGraphicsEllipseItem(QtCore.QRectF(self.lowerLeftCorner_xcoord, self.lowerLeftCorner_ycoord, 2*self.radius, 2*self.radius))        
         self.goodSegment.setStartAngle(self.startAngle)
         self.goodSegment.setSpanAngle(self.spanAngle)
-        self.complementarySegment = QtGui.QGraphicsEllipseItem(QtCore.QRectF(self.lowerLeftCorner_xcoord, self.lowerLeftCorner_xcoord, 2*self.radius, 2*self.radius))
-        self.complementarySegment.setPen(pg.mkPen(color = 'w',width=2.5))
-        # self.PlotWidgetIn_pageUHP.addItem(self.complementarySegment)
+        self.goodSegment.setPen(pg.mkPen(color=self.white, width=2))
+        self.complementarySegment = QtGui.QGraphicsEllipseItem(QtCore.QRectF(self.lowerLeftCorner_xcoord, self.lowerLeftCorner_ycoord, 2*self.radius, 2*self.radius))
         self.complementarySegment.setStartAngle(self.endAngle)
+        self.complementarySegment.setPen(pg.mkPen(color=self.black, width=2))
         self.complementarySegment.setSpanAngle(16*numpy.sign(self.spanAngle)*360-self.spanAngle)
 
 
-class line():
 
-    def __init__(self, startPoint, endPoint): 
-        self.line = QtGui.QGraphicsLineItem(startPoint.real, startPoint.imag, endPoint.real, endPoint.imag) # Do we need to add "QGraphicsItem *parent = nullptr" as the last parameter ?
+        self.blackPieces= [self.complementarySegment]
+        for c in ["white","blue","red","cyan","yellow","magenta","green"]:
+            if c == circSegmentColor:
+                exec("self."+c+"Pieces = [self.goodSegment]")
+            else:
+                exec("self."+c+"Pieces = []")
+            
+            
+
+
+
+
+
+ 
+
+
+        
+        
+        
+
+        
+        
